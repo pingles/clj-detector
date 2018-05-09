31fabf517a11732bceffa4b63610067d4fd7ea06
Ivan Grishaev <ivan@grishaev.me>
2018-05-09 16:35:43 +0300

    os field added

--- src/clj_detector/core.clj
+++ src/clj_detector/core.clj
@@ -1,9 +1,11 @@
 (ns clj-detector.core
   (:import [net.sf.uadetector.service UADetectorServiceFactory]
-           [net.sf.uadetector UserAgent UserAgentType VersionNumber DeviceCategory ReadableDeviceCategory$Category])
+           [net.sf.uadetector UserAgent UserAgentType VersionNumber
+            DeviceCategory ReadableDeviceCategory$Category
+            OperatingSystem])
   (:require [clojure.string :as st]))
 
-(defrecord Agent [name producer type version device])
+(defrecord Agent [name producer type version device os])
 
 (defprotocol ToClojure
   (to-clojure [x]))
@@ -15,40 +17,51 @@
   (to-clojure (.parse (parser) s)))
 
 (extend-protocol ToClojure
+
+  UserAgent
+  (to-clojure [agent]
+    (Agent. (-> agent .getName)
+            (-> agent .getProducer)
+            (-> agent .getType to-clojure)
+            (-> agent .getVersionNumber to-clojure)
+            (-> agent .getDeviceCategory to-clojure)
+            (-> agent .getOperatingSystem to-clojure)))
+
+  VersionNumber
+  (to-clojure [version]
+    (.toVersionString version))
+
+  OperatingSystem
+  (to-clojure [os]
+    {:name    (-> os .getName)
+     :family  (-> os .getFamilyName)
+     :version (-> os .getVersionNumber to-clojure)})
+
   DeviceCategory
   (to-clojure [device]
     (condp = (.getCategory device)
-      (ReadableDeviceCategory$Category/GAME_CONSOLE) :console
-      (ReadableDeviceCategory$Category/OTHER) :other
-      (ReadableDeviceCategory$Category/PDA) :pda
-      (ReadableDeviceCategory$Category/PERSONAL_COMPUTER) :pc
-      (ReadableDeviceCategory$Category/SMART_TV) :tv
-      (ReadableDeviceCategory$Category/SMARTPHONE) :phone
-      (ReadableDeviceCategory$Category/TABLET) :tablet
-      (ReadableDeviceCategory$Category/UNKNOWN) :unknown))
-  VersionNumber
-  (to-clojure [version]
-    (st/join "." (remove st/blank? (.getGroups version))))
-  UserAgent
-  (to-clojure [agent]
-    (Agent. (.getName agent)
-            (.getProducer agent)
-            (to-clojure (.getType agent))
-            (to-clojure (.getVersionNumber agent))
-            (to-clojure (.getDeviceCategory agent))))
+      ReadableDeviceCategory$Category/GAME_CONSOLE      :console
+      ReadableDeviceCategory$Category/OTHER             :other
+      ReadableDeviceCategory$Category/PDA               :pda
+      ReadableDeviceCategory$Category/PERSONAL_COMPUTER :pc
+      ReadableDeviceCategory$Category/SMART_TV          :tv
+      ReadableDeviceCategory$Category/SMARTPHONE        :phone
+      ReadableDeviceCategory$Category/TABLET            :tablet
+      ReadableDeviceCategory$Category/UNKNOWN           :unknown))
+
   UserAgentType
   (to-clojure [type]
     (condp = type
-      (UserAgentType/BROWSER) :browser
-      (UserAgentType/EMAIL_CLIENT) :email
-      (UserAgentType/FEED_READER) :feed-reader
-      (UserAgentType/LIBRARY) :library
-      (UserAgentType/MEDIAPLAYER) :media-player
-      (UserAgentType/MOBILE_BROWSER) :mobile-browser
-      (UserAgentType/OFFLINE_BROWSER) :offline-browser
-      (UserAgentType/OTHER) :other
-      (UserAgentType/ROBOT) :robot
-      (UserAgentType/UNKNOWN) :unknown
-      (UserAgentType/USERAGENT_ANONYMIZER) :anonymizer
-      (UserAgentType/VALIDATOR) :validator
-      (UserAgentType/WAP_BROWSER) :wap-browser)))
+      UserAgentType/BROWSER              :browser
+      UserAgentType/EMAIL_CLIENT         :email
+      UserAgentType/FEED_READER          :feed-reader
+      UserAgentType/LIBRARY              :library
+      UserAgentType/MEDIAPLAYER          :media-player
+      UserAgentType/MOBILE_BROWSER       :mobile-browser
+      UserAgentType/OFFLINE_BROWSER      :offline-browser
+      UserAgentType/OTHER                :other
+      UserAgentType/ROBOT                :robot
+      UserAgentType/UNKNOWN              :unknown
+      UserAgentType/USERAGENT_ANONYMIZER :anonymizer
+      UserAgentType/VALIDATOR            :validator
+      UserAgentType/WAP_BROWSER          :wap-browser)))
